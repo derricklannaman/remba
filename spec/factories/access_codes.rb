8c4820ff888ffbf34f4bdd856889b071c3e3ecae
Derrick Lannaman <derricklannaman@gmail.com>
2015-10-10 16:09:37 -0500

    light styling, and some added test files

--- spec/factories/access_codes.rb
+++ spec/factories/access_codes.rb
@@ -2,17 +2,17 @@
 #
 # Table name: access_codes
 #
-#  id          :integer          not null, primary key
-#  access_code :integer
-#  stylist_id  :integer
-#  team_id     :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id             :integer          not null, primary key
+#  stylist_id     :integer
+#  team_id        :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  connection_key :string
 #
 
 FactoryGirl.define do
   factory :access_code do
-    access_code 1
+    access_code "MyString"
 stylist_id 1
 team_id 1
   end
--- spec/models/access_code_spec.rb
+++ spec/models/access_code_spec.rb
@@ -2,12 +2,12 @@
 #
 # Table name: access_codes
 #
-#  id          :integer          not null, primary key
-#  access_code :integer
-#  stylist_id  :integer
-#  team_id     :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id             :integer          not null, primary key
+#  stylist_id     :integer
+#  team_id        :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  connection_key :string
 #
 
 require 'rails_helper'
