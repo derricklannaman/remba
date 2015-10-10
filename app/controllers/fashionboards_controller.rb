ac91748cb43f2ee98fe25243163cb2513988962e
Derrick Lannaman <derricklannaman@gmail.com>
2015-10-10 17:06:51 -0500

    refactored into smaller methods and cleaned up some code

--- app/controllers/fashionboards_controller.rb
+++ app/controllers/fashionboards_controller.rb
@@ -1,19 +1,34 @@
 class FashionboardsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:show]
 
   def show
-    # team_count
-    # binding.pry
-    @stylists = current_member.team.access_codes.map(&:stylist_id).map {|id|Stylist.find(id)}
-    team_count
+    @stylists = teams_stylists
+    team_stylist_count
   end
 
 
   private
 
-    def team_count
-      @team_count = @stylists.size
-      # @team_count = current_member.team.count
+    def teams_stylists
+      team_access_codes.map(&:stylist_id).map {|id|Stylist.find id }
+    end
+
+    def team_stylist_count
+      fashion_team.count = stylist_count
+      fashion_team.save
+      @team_count = current_member.team.count
+    end
+
+    def stylist_count
+      @stylists.size
+    end
+
+    def team_access_codes
+      fashion_team.access_codes
+    end
+
+    def fashion_team
+      current_member.team
     end
 
 
