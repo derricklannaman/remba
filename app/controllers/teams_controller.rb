121d4577c0df9ddec333fe431b853b62f535c9f9
Derrick Lannaman <derricklannaman@gmail.com>
2015-10-13 13:45:10 -0500

    create method to restrict a team to only 3 stylist

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,9 +6,18 @@ class TeamsController < ApplicationController
   def add_stylist
     team_id = current_member.team.id
     stylist_id = params[:id].to_i
-    AccessCode.create(team_id: team_id, stylist_id: stylist_id)
+
+    add_to_team_if_team_not_full team_id, stylist_id
 
     redirect_to fashionboard_path
   end
+
+  private
+
+    def add_to_team_if_team_not_full team_id, stylist_id
+      if current_member.team.count < 3
+        AccessCode.create(team_id: team_id, stylist_id: stylist_id)
+      end
+    end
 
 end
--- app/helpers/fashionboards_helper.rb
+++ app/helpers/fashionboards_helper.rb
@@ -1,12 +1,11 @@
 module FashionboardsHelper
 
   def disable_add_stylist_if_greater_than_3
-    if @team_count < 3
-      puts 'greater than three'
+    # if @team_count < 3
       link_to "Add Stylist", stylists_path, class: "btn btn-warning"
-    else
-      link_to "Add Stylist", stylists_path, class: "btn btn-default disabled"
-    end
+    # else
+      # link_to "Add Stylist", stylists_path, class: "btn btn-default disabled"
+    # end
   end
 
 end
