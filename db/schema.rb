b83ad8899999c2d2ae093ae62bf12bd923c9e10e
Derrick Lannaman <derricklannaman@gmail.com>
2015-10-21 17:11:26 -0500

    remove uniq on follows and replace index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018234623) do
+ActiveRecord::Schema.define(version: 20151021215235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20151018234623) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "follows", ["target_id", "user_id"], name: "index_follows_on_target_id_and_user_id", unique: true, using: :btree
+  add_index "follows", ["target_id", "user_id"], name: "index_follows_on_target_id_and_user_id", using: :btree
   add_index "follows", ["target_id"], name: "index_follows_on_target_id", using: :btree
   add_index "follows", ["user_id"], name: "index_follows_on_user_id", using: :btree
 
