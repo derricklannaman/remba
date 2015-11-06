6c009586beb024f760e5681ccb8791889f614669
Derrick Lannaman <derricklannaman@gmail.com>
2015-11-06 00:10:18 -0600

    add method to display feedback count via icon

--- app/helpers/channel_helper.rb
+++ app/helpers/channel_helper.rb
@@ -1,5 +1,5 @@
 module ChannelHelper
-  def diplay_pinned_image activity
+  def display_pinned_image activity
     attachment_url(activity['object'], :item_image, :fill, 300, 300)
   end
 
@@ -7,4 +7,15 @@ module ChannelHelper
     feedback = activity['object'].feedbacks
     feedbacks = feedback.group_by(&:name).map {|name, feedback| [name, feedback.length]}
   end
+
+  def display_feedback_counter_icon feedback_type
+    if feedback_type.first == "Like it"
+      content_tag(:i, feedback_type.last, class: ['fa', 'fa-check', 'margin-spacer'])
+    elsif feedback_type.first == "Love it"
+      content_tag(:i, feedback_type.last, class: ['fa', 'fa-heart', 'margin-spacer'])
+    elsif feedback_type.first == "Leave it"
+      content_tag(:i, feedback_type.last, class: ['fa', 'fa-thumbs-down', 'margin-spacer'])
+    end
+  end
+
 end
