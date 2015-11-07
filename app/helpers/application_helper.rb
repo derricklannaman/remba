3602e871e064faba279be69f041ff6e8a79616be
Derrick Lannaman <derricklannaman@gmail.com>
2015-11-06 18:27:03 -0600

    CSS 	- add mixing, site-wide file 	- reorganize and improve fashion item card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def when_pinned activity
     pin_id = activity['foreign_id'].split(':').last
     pin = Pin.find(pin_id)
-    time_ago_in_words pin.created_at
+    "#{time_ago_in_words pin.created_at} ago"
   end
 
   def display_item_image item
