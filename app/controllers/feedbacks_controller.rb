b54880fe69bb9646df4f3c7c8549b8963323154f
Derrick Lannaman <derricklannaman@gmail.com>
2015-11-16 16:39:50 -0600

    Clean up feedback controller, remove model from form tidy up css

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,29 +1,18 @@
 class FeedbacksController < ApplicationController
 
-  def new
-    @feedback = Feedback.new
-  end
-
   def create
-    if params.has_key? :feedback
-      feedback = Feedback.new feedback_params
-      feedback.name = params[:feedback][:name]
+    if params.keys[3].present?
+      feedback = Feedback.new
+      feedback.name = params.values[3]
       feedback.item_id = params[:item_id]
       feedback.user_id = current_user.id
       if feedback.save
-        redirect_to :back, alert: "feedback sent!"
+        redirect_to :back, alert: "Your feedback was sent."
       end
     else
       redirect_to :back, alert: "Oops! You submitted without giving feedback.
                                  Please provide feedback first."
     end
   end
-
-  private
-
-  def feedback_params
-    params.require(:feedback).permit(:item_id, :feedback, :name)
-  end
-
 
 end
--- app/helpers/channel_helper.rb
+++ app/helpers/channel_helper.rb
@@ -1,4 +1,3 @@
-
 module ChannelHelper
   def display_pinned_image activity
     attachment_url(activity['object'], :item_image, :fill, 300, 300)
