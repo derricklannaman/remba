09100cfd8fe0fce6200705d53f5d7589c17865f3
Derrick Lannaman <derricklannaman@gmail.com>
2015-11-22 22:57:30 -0600

    create methods to check if feedback is valid, fixing feedback count is ongoing

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,7 +1,7 @@
 class FeedbacksController < ApplicationController
 
   def create
-    if params.keys[3].present?
+    if feedback_name_is_present? && feedback_valid?
       feedback = Feedback.new
       feedback.name = params.values[3]
       feedback.item_id = params[:item_id]
@@ -13,6 +13,35 @@ class FeedbacksController < ApplicationController
       redirect_to :back, alert: "Oops! You submitted without giving feedback.
                                  Please provide feedback first."
     end
+  end
+
+  def feedback_name_is_present?
+    params.keys[3].present?
+  end
+
+  def feedback_valid?
+     like_it_valid? || love_it_valid? || leave_it_valid?
+  end
+
+  def like_it_valid?
+    params.values[3] == "Like it"
+  end
+
+  def love_it_valid?
+    params.values[3] == "Love it"
+  end
+
+  def leave_it_valid?
+    params.values[3] == "Leave it"
+  end
+
+  def destroy
+    selected_feedback = Feedback.find(params[:id])
+    feedbacks = Feedback.where(user_id: current_user.id)
+    feedbacks.each do |feedback|
+      feedback.destroy if feedback == selected_feedback
+    end
+    redirect_to :back
   end
 
 end
--- app/helpers/channel_helper.rb
+++ app/helpers/channel_helper.rb
@@ -1,4 +1,7 @@
 module ChannelHelper
+
+  private
+
   def display_pinned_image activity
     attachment_url(image_type(activity), :item_image, :fill, 300, 300)
   end
@@ -11,30 +14,50 @@ module ChannelHelper
     @feedback = image_type(activity).feedbacks
     @like_count, @love_count, @leave_count = @feedback.group_by(&:name)
                                 .map {|name, feedback| [name, feedback.size]}
+# binding.pry
+    # @feedback.group_by(&:name).map {|name, feedback| [name, feedback.size]}
+    # @feedback.group_by(&:name).map {|name, feedback| [name, feedback.size]}.each do |feedback|
+    # #   # @like_count = feedback if feedback.first == "Like it"
+    # #   # @love_count = feedback if feedback.first == "Love it"
+    # #   # @leave_count = feedback if feedback.first == "Leave it"
+    # binding.pry
+    #   if feedback.first == "Like it"
+    #     @like_count = feedback
+    #   elsif feedback.first == "Love it"
+    #     @love_count = feedback
+    #   elsif feedback.first == "Leave it"
+    #     @leave_count = feedback
+    #   end
+    # end
   end
 
   def like_it_count
-    @like_count.present? ? @like_count : ['Like it', 0]
+    @like_count.present? ? @like_count : ['Like it', '0']
   end
 
   def love_it_count
-    @love_count.present? ? @love_count : ['Love it', 0]
+    @love_count.present? ? @love_count : ['Love it', '0']
   end
 
   def leave_it_count
-    @leave_count.present? ? @leave_count : ['Leave it', 0]
+    @leave_count.present? ? @leave_count : ['Leave it', '0']
   end
 
   def display_feedback_counter_icon feedback_type
-    # TODO: refactor this shit
+    # binding.pry
     case feedback_type.first
-      when "Like it"
-        content_tag(:i, content_tag(:span, feedback_type.last), class: "fa fa-thumbs-o-up margin-spacer")
-      when "Love it"
-        content_tag(:i, content_tag(:span, feedback_type.last), class: "fa fa-heart-o margin-spacer")
-      when "Leave it"
-        content_tag(:i, content_tag(:span, feedback_type.last), class: "fa fa-thumbs-o-down margin-spacer")
+    when "Like it"
+      select_icon "fa-thumbs-o-up", feedback_type
+    when "Love it"
+      select_icon "fa-heart-o", feedback_type
+    when "Leave it"
+      select_icon "fa-thumbs-o-down", feedback_type
     end
+  end
+
+  def select_icon icon, feedback_type
+    # binding.pry
+    content_tag(:i, content_tag(:span, feedback_type.last), class: "fa #{icon} margin-spacer") #unless feedback_type.last == 0
   end
 
 
