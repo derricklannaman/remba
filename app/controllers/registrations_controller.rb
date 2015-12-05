5e94cb4385f106f4fdb255952e537781e8350562
Derrick Lannaman <derricklannaman@gmail.com>
2015-12-04 19:00:45 -0600

    fix sign in form params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,33 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def new
-    # binding.pry
     @user = User.new
   end
 
   def create
-    if a_stylist?
-      # @user = Stylist.new(user_params)
-      @user = User.new(user_params)
+    if stylist_sign_up?
+      new_sign_up_user
       @user.status = "Stylist"
-    elsif a_member?
-      # @user = Member.new(user_params)
-      @user = User.new(user_params)
+    elsif member_sign_up?
+      new_sign_up_user
       @user.status = "Member"
+    else
+      default_user
     end
     save_and_redirect_user
   end
 
-
   private
+
+  def new_sign_up_user
+    @user = User.new(user_params)
+  end
+
+  def default_user
+    params[:user][:stylist] = 0
+    new_sign_up_user
+    @user.status = "Member"
+  end
 
   def save_and_redirect_user
     @user.save
@@ -30,16 +38,20 @@ class RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  def a_member?
-    params[:user][:stylist] == '0'
+  def member_sign_up?
+    if params[:user][:stylist] == 'no'
+      params[:user][:stylist] = 0
+    end
   end
 
-  def a_stylist?
-    params[:user][:stylist] == '1'
+  def stylist_sign_up?
+    if params[:user][:stylist].downcase == 'yes'
+      params[:user][:stylist] = 1
+    end
   end
 
   def user_params
-    params.require(:user).permit(:stylist, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :stylist, :email, :password, :password_confirmation)
   end
 
 end
