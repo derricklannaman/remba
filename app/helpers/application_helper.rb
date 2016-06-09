efb261f41b5ea424ec8f56b8cd91b8925386909a
Derrick Lannaman <derricklannaman@gmail.com>
2016-06-09 14:26:03 -0500

    refactor profile images into its own module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  include ProfileImagesHelper
 
   def time_of_posting activity
     pin_id = activity['foreign_id'].split(':').last
@@ -25,35 +26,6 @@ module ApplicationHelper
 
   def full_name
     "#{first_name} #{last_name}"
-  end
-
-  def profile_picture
-    if no_profile_image_exist?
-      image_tag('default-profile.png', size: "200x200", class: 'default-profile')
-    else
-      attachment_image_tag(current_user, :profile_image, :fill, 200, 200,
-                                  format: "jpg", fallback: "default-profile.png")
-    end
-  end
-
-  def minisize_thumbnail_current_user
-    if no_profile_image_exist?
-      link_to image_tag('default-profile.png', size: "200x200", class: 'default-profile')
-    else
-    link_to attachment_image_tag(current_user, :profile_image, :fill, 40, 40,
-                                format: "jpg", fallback: "default-profile.png"),
-                                profile_path unless no_profile_image_exist?
-    end
-  end
-
-  def midsize_thumbnail_user user
-    link_to attachment_image_tag(user, :profile_image, :fill, 60, 60,
-                                format: "jpg", fallback: "", class: "middle-thumbnail"),
-                                user_path(user.id)
-  end
-
-  def no_profile_image_exist?
-    current_user.profile_image_id.nil?
   end
 
 end
---
+++ app/helpers/profile_images_helper.rb
@@ -0,0 +1,43 @@
+module ProfileImagesHelper
+
+  def profile_picture
+    if profile_image_exist?(current_user)
+      user_image(current_user, 200, "default-profile")
+    else
+      generic_profile_image(200, "default-profile")
+    end
+  end
+
+  def current_user_profile_image user, img_size, css_style
+    if profile_image_exist?(user)
+      link_to user_image(user, img_size, css_style), profile_path
+    else
+      link_to generic_profile_image(img_size, "default-profile"), profile_path
+    end
+  end
+
+  def user_profile_image user, img_size, css_style
+    if profile_image_exist?(user)
+      if user == current_user
+        link_to user_image(user, img_size, css_style), profile_path
+      else
+        link_to user_image(user, img_size, css_style), user_path(user.id)
+      end
+    else
+      link_to generic_profile_image(img_size, "fallback-profile"), user_path(user.id)
+    end
+  end
+
+  def user_image user, img_size, css_style
+    attachment_image_tag(user, :profile_image, :fill,"#{img_size}","#{img_size}",
+                              format: "jpg", class: "#{css_style}", fallback: "default-profile.png")
+  end
+
+  def generic_profile_image img_size, placeholder_image_style
+    image_tag('default-profile.png', size: "#{img_size}x#{img_size}", class: "#{placeholder_image_style}")
+  end
+
+  def profile_image_exist? user
+    user.profile_image_id.present?
+  end
+end
